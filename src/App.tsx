184a5039a58e77244fdf1ab0d2424c36ee417af4
Jonathan Sandman <jonathansandman@outlook.com>
2021-03-08 16:55:48 +0000

    Implement basic truncator app

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Truncator } from "./Truncator";
+import { Input, Layout, Menu } from "antd";
+import { Content, Header } from "antd/lib/layout/layout";
 
 function App() {
+  const [text, setText] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Layout className="fill">
+      <Header className="header">
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["1"]}>
+          <Menu.Item key="1">Truncator</Menu.Item>
+        </Menu>
+      </Header>
+      <Content>
+        <div
+          style={{
+            display: "flex",
+            height: "100%",
+            width: "100%",
+            padding: 8,
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <div
+            style={{
+              width: "50%",
+              height: "100%",
+              padding: 8,
+              boxSizing: "border-box",
+              overflow: "hidden",
+            }}
+          >
+            <div style={{ marginBottom: 8 }}>INPUT</div>
+            <Input.TextArea
+              value={text}
+              onChange={(e) => setText(e.currentTarget.value)}
+              style={{
+                resize: "none",
+                width: "100%",
+                height: "95%",
+              }}
+            />
+          </div>
+          <div style={{ width: "50%", height: "50%", padding: 8 }}>
+            <div>
+              <div style={{ marginBottom: 8 }}>OUTPUT</div>
+              <div style={{ display: "flex" }}>
+                <div style={{ flex: 1 }}>Original</div>
+                <div style={{ flex: 0.75 }}>8</div>
+                <div style={{ flex: 0.5 }}>4</div>
+              </div>
+              {text
+                .split(/\r?\n/)
+                .filter((x) => x.trim().length > 0)
+                .map((t) => (
+                  <Truncator text={t} />
+                ))}
+            </div>
+          </div>
+        </div>
+      </Content>
+    </Layout>
   );
 }
 
---
+++ src/Truncator.tsx
@@ -0,0 +1,117 @@
+import { FunctionComponent } from "react";
+
+interface IProps {
+  text: string;
+}
+
+export const Truncator: FunctionComponent<IProps> = ({ text }) => {
+  const full = text;
+  const char8 = TruncateTo8Characters(text);
+  const char4 = TruncateTo4Characters(text);
+  return (
+    <div style={{ display: "flex", fontFamily: "monospace" }}>
+      <div
+        style={{
+          color: ContainsPunctuation(full) ? "red" : undefined,
+          padding: 8,
+          flex: 1,
+          border: "1px solid grey",
+        }}
+      >
+        {full}
+      </div>
+      <div
+        style={{
+          color: ContainsPunctuation(char8) ? "red" : undefined,
+          padding: 8,
+          flex: 0.75,
+
+          border: "1px solid grey",
+        }}
+      >
+        {char8}
+      </div>
+      <div
+        style={{
+          color: ContainsPunctuation(char4) ? "red" : undefined,
+          padding: 8,
+          flex: 0.5,
+          border: "1px solid grey",
+        }}
+      >
+        {char4}
+      </div>
+    </div>
+  );
+};
+
+function TruncateTo8Characters(str: string): string {
+  var newString = RemovePunctuation(str.trim());
+  var words = str.split(/(\s)/);
+
+  wordsToRemove.forEach((wordToRemove) => {
+    newString = newString.replace(wordToRemove, "");
+  });
+
+  newString = newString.replace(/\s+/, "");
+
+  var sortedWords = words.sort((a, b) => (a.length > b.length ? -1 : 1));
+
+  sortedWords.forEach((word) => {
+    if (newString.length > 8) {
+      newString = newString.replace(word, RemoveVowels(word));
+    }
+  });
+
+  return newString;
+}
+
+function TruncateTo4Characters(str: string): string {
+  var newString = RemovePunctuation(str.trim());
+  var words = str.split(/\s/);
+
+  wordsToRemove.forEach((wordToRemove) => {
+    newString = newString.replace(wordToRemove, "");
+  });
+
+  var sortedWords = words.sort((a, b) => (a.length > b.length ? -1 : 1));
+
+  sortedWords.forEach((word) => {
+    if (newString.length > 4) {
+      newString = newString.replace(word, RemoveVowels(word));
+    }
+  });
+
+  var sortedTruncatedWords = newString
+    .split(/\s/)
+    .sort((a, b) => (a.length > b.length ? -1 : 1))
+    .map((x) => x.trim())
+    .filter((x) => x.length !== 0);
+
+  var shortestWord = sortedTruncatedWords.slice(-1)[0];
+
+  newString = newString.replace(/\s+/gi, "");
+
+  sortedTruncatedWords.forEach((word) => {
+    if (newString.length > 4) {
+      var newWord = word;
+      newString = newString.replace(word, newWord);
+    }
+  });
+
+  return newString.split("").slice(0,4).join("");
+}
+
+function RemoveVowels(str: string) {
+  return str.replace(/[aeiou]/g, "");
+}
+
+function ContainsPunctuation(str: string) {
+  return str.match(/[^\w\s]/gi) != null;
+}
+
+function RemovePunctuation(str: string) {
+  return str.replace(/[^\w\s]/gi, "");
+}
+
+const wordsToRemove = ["and"];
